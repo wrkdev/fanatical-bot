829d071a1d15091b1a73d31cb749ac94da6241d4
wrkdev <wrkdev12@gmail.com>
2020-06-07 13:39:38 +0100

    master: Added embedded messages for help guides. Main help guide still work in progress.

--- commands/help.js
+++ commands/help.js
@@ -1,18 +1,50 @@
 const { prefix } = require('../config/config.json');
+const Discord = require('discord.js');
 
 module.exports = {
 	name: 'help',
 	description: 'List all of my commands or info about a specific command.',
-	aliases: ['commands'],
-	usage: '[command name]',
+	aliases: ['help'],
+    usage: 'f!help [command]',
+    inHelp: 'yes',
 	cooldown: 5,
 	execute(message, args) {
+
+        function embedAllCommands(object) {
+            let embedMessage = new Discord.MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle("Fanatical's Bot Command List")
+                .setThumbnail('https://cdn.discordapp.com/avatars/718008997797691422/54bf8ba8436a8fd10489691294d76bfc.png')
+                .setDescription('Information on list of available commands');
+            const argumentArray = object.map(command => command);
+            argumentArray.forEach(commandInfo => {
+                embedMessage.addFields(
+                    { name: 'Command', value: commandInfo.name, inline: false },
+                    { name: 'Description', value: commandInfo.description, inline: false }
+                )
+            });
+            embedMessage.setTimestamp()
+            .setFooter(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`, 'https://cdn.discordapp.com/avatars/718008997797691422/54bf8ba8436a8fd10489691294d76bfc.png');
+            return embedMessage;
+        }
+
+        function turnToEmbed(object) {
+            let embedMessage = new Discord.MessageEmbed()
+                .setColor('RANDOM')
+                .setTitle(`Command Info about: *${object.name}*`)
+                .setDescription(object.description)
+                .setTimestamp();
+                if (object.usage) embedMessage.addField('Usage:', object.usage, true);
+                if (object.aliases) embedMessage.addField('Aliases:', object.aliases.join(', '), true);
+                embedMessage.addField('Cooldown (seconds)', command.cooldown || 3, true)
+            return embedMessage;
+        }
+
         const data = [];
         const { commands } = message.client;
         if (!args.length) {
             data.push('Here\'s a list of all my commands:');
-            data.push(commands.map(command => command.name).join(', '));
-            data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
+            data.push(embedAllCommands(commands));
 
             return message.author.send(data, { split: true })
                 .then(() => {
@@ -31,15 +63,7 @@ module.exports = {
         if (!command) {
             return message.reply('that\'s not a valid command!');
         }
-
-        data.push(`**Name:** ${command.name}`);
-
-        if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
-        if (command.description) data.push(`**Description:** ${command.description}`);
-        if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
-
-        data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
-
+        data.push(turnToEmbed(command));
         message.channel.send(data, { split: true });
 	},
 };
